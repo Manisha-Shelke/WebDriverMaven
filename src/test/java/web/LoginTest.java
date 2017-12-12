70946645248a39a9e50e75b3140a8cf350e5c544
unknown <manisha.shelke123@gmail.com>
2017-12-12 17:59:07 +0530

    Initioal Commit

---
+++ src/test/java/web/LoginTest.java
@@ -0,0 +1,37 @@
+package web;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+WebDriver driver;
+	
+	@BeforeTest
+	public void setup(){
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\SPARTEN\\Desktop\\workspace\\ChromeDriver\\chromedriver_win32\\chromedriver.exe");
+ driver=new ChromeDriver();
+	}
+
+
+     @Test
+     public void doLogin(){
+	driver.get("http://facebook.com");
+	
+	driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("manisha.shelke123@gmail.com");
+	driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("mani@Prafull");
+	//element=driver.findElement(By.xpath(".//*[@id='pass']"));
+	//element.click();
+	}
+	
+     @AfterTest
+     public void closeDriver(){
+    	 driver.close();
+     }
+	
+
+}
